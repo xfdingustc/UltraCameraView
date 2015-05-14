cc310fa027db26d2b223f4df24221aa7a8cd7d9a
xfding <xfdingustc@gmail.com>
2015-05-14 14:14:38 +0800

    add new module: camera

---
+++ camera/src/main/java/com/xfdingustc/camera/UltraCameraView.java
@@ -0,0 +1,19 @@
+package com.xfdingustc.camera;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Xiaofei on 2015/5/14.
+ */
+public class UltraCameraView extends ViewGroup {
+  public UltraCameraView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override
+  protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+  }
+}
